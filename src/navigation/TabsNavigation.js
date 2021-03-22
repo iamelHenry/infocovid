04fe13535b208c727246087c784430853673f085
Henry Troncoso <henry.troncoso@cencosud.cl>
2021-03-22 08:16:20 -0300

    :construction_worker: tabbar icons

--- src/navigation/TabsNavigation.js
+++ src/navigation/TabsNavigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeScreenStack from '../screens/Home';
 import NavigateScreenStack from '../screens/Navigate';
 import LocationScreenStack from '../screens/Location';
@@ -15,10 +16,43 @@ export default function TabsNavigation() {
       <Tab.Screen
         name="Home"
         component={ HomeScreenStack }
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          )
+        }}
         />
-      <Tab.Screen name="Navegar" component={ NavigateScreenStack } />
-      <Tab.Screen name="Comuna" component={ LocationScreenStack } />
-      <Tab.Screen name="Favoritos" component={ FavoriteScreenStack } />
+      <Tab.Screen
+        name="Navegar"
+        component={ NavigateScreenStack }
+        options={{
+          tabBarLabel: 'Navegar',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="map" color={color} size={size} />
+          )
+        }}
+        />
+      <Tab.Screen
+        name="Comuna"
+        component={ LocationScreenStack }
+        options={{
+          tabBarLabel: 'Comuna',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="map-marker-radius" color={color} size={size} />
+          )
+        }}
+        />
+      <Tab.Screen
+        name="Favoritos"
+        component={ FavoriteScreenStack }
+        options={{
+          tabBarLabel: 'Favoritos',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="star" color={color} size={size} />
+          )
+        }}
+        />
     </Tab.Navigator>
   );
 };
